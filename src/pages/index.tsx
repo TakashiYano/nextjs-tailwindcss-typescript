fcba5c50d0927dbc89825da8043231b125f9a62d
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-12-22 02:23:14 +0000

    🚨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,13 @@ import { Button } from "src/components/Button";
 import { Layout } from "src/components/Layout";
 
 const Home = () => {
+  const handleClick = () => {
+    window.alert("Hello World!");
+  };
+
   return (
     <Layout>
-      <Button
-        button
-        className="px-5 py-2 font-bold"
-        variant="solid"
-        onClick={() => {
-          window.alert("Hello World!");
-        }}
-      >
+      <Button button className="px-5 py-2 font-bold" variant="solid" onClick={handleClick}>
         Button
       </Button>
     </Layout>
