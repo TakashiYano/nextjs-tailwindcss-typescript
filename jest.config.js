4bc87b06db98909565935fbeae80b0133ab38b78
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-12-26 13:49:16 +0000

    ✅ テスト環境のアップデート

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-  roots: ["<rootDir>"],
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
-  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-  transform: { "^.+\\.(ts|tsx)$": "babel-jest" },
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  testEnvironment: "jsdom",
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
   moduleNameMapper: {
-    "^.+\\.module\\.(css|scss)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/test/__mocks__/fileMock.js",
+    // Handle CSS inports (with CSS modules): https://jestjs.io/docs/webpack#mocking-css-modules
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    // Handle CSS imports (without CSS modules)
+    "^.+(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    // Handle image imports: https://jestjs.io/docs/webpack#handling-static-assets
+    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/__mocks__/fileMock.js`,
+    // Absolute Imports and Module Path Aliases
     "src/(.*)": "<rootDir>/src/$1",
     "test/(.*)": "<rootDir>/test/$1",
   },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    // User babel-jest to transpile tests with the next/babel preset: https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presents: ["next/babael"] }],
+  },
+  transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
