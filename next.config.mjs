4a43c29a62cf36ef5c639bf13f168dde61e7718f
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-12-28 02:41:33 +0000

    🩹

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,12 @@
  * @types {import('next').NextConfig}
  **/
 const nextConfig = {
-  rewrites: async () => {
-    return [{ source: "/", destination: "/root" }];
-  },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   future: { strictPostcssConfiguration: true },
-  // pageExtensions: ["page.tsx", "page.ts"],
   reactStrictMode: true,
   poweredByHeader: false,
-  experimental: { concurrentFeatures: true, serverComponents: true },
-  swcMinify: false,
+  swcMinify: true,
+  // experimental: { concurrentFeatures: true, serverComponents: true },
 };
 
 export default nextConfig;
