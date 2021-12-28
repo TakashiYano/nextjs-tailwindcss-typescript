5e29ac8a21c8500e5451d8ca4bc6273e6239a0f1
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-12-28 04:08:28 +0000

    🚚

---
+++ src/pages/about.tsx
@@ -0,0 +1,5 @@
+import { VFC } from "react";
+
+export const About: VFC = () => {
+  return <div>About!</div>;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { VFC } from "react";
+
+export const Index: VFC = () => {
+  const handleClick = () => {
+    window.alert("Hello, World!");
+  };
+
+  return (
+    <div>
+      <div>Index</div>
+      <button className="p-2" onClick={handleClick}>
+        Click me!
+      </button>
+    </div>
+  );
+};
